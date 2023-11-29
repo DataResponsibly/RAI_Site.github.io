3ecff8935badf12371293818f0ac01d1dd729029
Lucas Rosenblatt <lucas.rosenblatt@nyu.edu>
2023-11-28 21:03:42 -0500

    fixed banner swap light dark

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,32 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const mode_toggle = document.getElementById("light-toggle");
+
+//     mode_toggle.addEventListener("click", function() {
+//         toggleTheme(localStorage.getItem("theme"));
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
     const mode_toggle = document.getElementById("light-toggle");
+    const banner = document.getElementById("banner");
 
     mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
+        const currentTheme = localStorage.getItem("theme");
+        toggleTheme(currentTheme);
+        toggleBannerClass(currentTheme);
     });
 });
 
+function toggleBannerClass(currentTheme) {
+    const banner = document.getElementById("banner");
+
+    if (banner) {
+        if (currentTheme === 'dark') {
+            banner.classList.add('banner-light');
+            banner.classList.remove('banner-dark');
+        } else {
+            banner.classList.add('banner-dark');
+            banner.classList.remove('banner-light');
+        }
+    }
+}
