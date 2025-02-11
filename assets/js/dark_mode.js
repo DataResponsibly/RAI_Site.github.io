5ea6b594cf10d158b37a62f196ce4efe8adbb55a
lurosenb <60412438+lurosenb@users.noreply.github.com>
2025-02-11 01:27:35 +0000

    Deploying to gh-pages from @ DataResponsibly/RAI_Site.github.io@3f6b8bb40dc0c9af91d77a3de3a722aa0f60094a 🚀

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,1 +1,48 @@
-function loadCorrect(){const e=localStorage.getItem("theme"),t=document.getElementById("banner");"light"===e?(t.classList.add("banner-light"),t.classList.remove("banner-dark")):(t.classList.add("banner-dark"),t.classList.remove("banner-light"))}function toggleBannerClass(e){const t=document.getElementById("banner");t&&("dark"===e?(t.classList.add("banner-light"),t.classList.remove("banner-dark")):(t.classList.add("banner-dark"),t.classList.remove("banner-light")))}document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("light-toggle");document.getElementById("banner");e.addEventListener("click",function(){const e=localStorage.getItem("theme");toggleTheme(e),toggleBannerClass(e)})}),window.addEventListener("load",setTimeout(loadCorrect,1));
+// document.addEventListener('DOMContentLoaded', function() {
+//     const mode_toggle = document.getElementById("light-toggle");
+
+//     mode_toggle.addEventListener("click", function() {
+//         toggleTheme(localStorage.getItem("theme"));
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mode_toggle = document.getElementById("light-toggle");
+    const banner = document.getElementById("banner");
+
+    mode_toggle.addEventListener("click", function() {
+        const currentTheme = localStorage.getItem("theme");
+        toggleTheme(currentTheme);
+        toggleBannerClass(currentTheme);
+    });
+});
+
+window.addEventListener("load", setTimeout(loadCorrect, 1)); 
+
+function loadCorrect() {
+    const currentTheme = localStorage.getItem("theme");
+    const banner = document.getElementById("banner");
+    if (currentTheme === 'light') {
+        banner.classList.add('banner-light');
+        banner.classList.remove('banner-dark');
+    } else {
+        banner.classList.add('banner-dark');
+        banner.classList.remove('banner-light');
+    }
+
+}
+
+
+function toggleBannerClass(currentTheme) {
+    const banner = document.getElementById("banner");
+
+    if (banner) {
+        if (currentTheme === 'dark') {
+            banner.classList.add('banner-light');
+            banner.classList.remove('banner-dark');
+        } else {
+            banner.classList.add('banner-dark');
+            banner.classList.remove('banner-light');
+        }
+    }
+}
